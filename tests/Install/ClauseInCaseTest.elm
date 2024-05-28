bb7176e7e71f045183674a11478e44b7630f5351
Mateus <mateusfpleite@gmail.com>
2024-05-28 08:12:00 -0300

    fix: adjust ClauseInCaseTest to new module format

--- tests/Install/ClauseInCaseTest.elm
+++ tests/Install/ClauseInCaseTest.elm
@@ -1,12 +1,17 @@
 module Install.ClauseInCaseTest exposing (all)
 
-import Install.ClauseInCase exposing (rule)
+import Install.ClauseInCase exposing (init, makeRule)
 import Review.Test
 import Test exposing (Test, describe, test)
 
 
 all : Test
 all =
+    let
+        rule =
+            init "REPLACEME" "REPLACEME" "REPLACEME" "REPLACEME"
+                |> makeRule
+    in
     describe "Install.ClauseInCase"
         [ test "should not report an error when REPLACEME" <|
             \() ->
