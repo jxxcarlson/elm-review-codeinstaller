9e2e630e62b8bd7dd1dd2364e3a4666fd15eca4f
James Carlson <jxxcarlson@gmail.com>
2024-05-31 18:15:23 +0200

    Refactor ClauseInCaseTest2

--- tests/Install/ClauseInCaseTest2.elm
+++ tests/Install/ClauseInCaseTest2.elm
@@ -1,21 +1,42 @@
 module Install.ClauseInCaseTest2 exposing (all)
 
+import Expect exposing (Expectation)
 import Install.ClauseInCase
+import Review.Rule exposing (Rule)
 import Review.Test
 import Test exposing (Test, describe, test)
 
 
 all : Test
 all =
-    let
-        rule =
-            Install.ClauseInCase.init "Backend" "updateFromFrontend" "ResetCounter" "( { model | counter = 0 }, broadcast (CounterNewValue 0 clientId) )"
-                |> Install.ClauseInCase.makeRule
-    in
     describe "Install.ClauseInCase"
-        [ test "should report an error and fix it" <|
-            \() ->
-                """module Backend exposing (..)
+        [ makeTest "Test 1: should report an error and fix it" src1 rule1 under1 "Add handler for ResetCounter"
+        ]
+
+
+makeTest : String -> String -> Rule -> String -> String -> Test
+makeTest description src rule under message =
+    test description <|
+        \() ->
+            src
+                |> Review.Test.run rule
+                |> Review.Test.expectErrors
+                    [ Review.Test.error { message = message, details = [ "" ], under = under }
+                        |> Review.Test.whenFixed fixed1
+                    ]
+
+
+
+-- TEST 1
+
+
+rule1 =
+    Install.ClauseInCase.init "Backend" "updateFromFrontend" "ResetCounter" "( { model | counter = 0 }, broadcast (CounterNewValue 0 clientId) )"
+        |> Install.ClauseInCase.makeRule
+
+
+src1 =
+    """module Backend exposing (..)
 
 updateFromFrontend : SessionId -> ClientId -> ToBackend -> Model -> ( Model, Cmd BackendMsg )
 updateFromFrontend sessionId clientId msg model =
@@ -29,16 +50,10 @@ updateFromFrontend sessionId clientId msg model =
 
 
 """
-                    |> Review.Test.run rule
-                    |> Review.Test.expectErrors
-                        [ Review.Test.error { message = "Add handler for ResetCounter", details = [ "" ], under = """case msg of
-         CounterIncremented ->
-            let
-                newCounter =
-                    model.counter + 1
-            in
-            ( { model | counter = newCounter }, broadcast (CounterNewValue newCounter clientId) )""" }
-                            |> Review.Test.whenFixed """module Backend exposing (..)
+
+
+fixed1 =
+    """module Backend exposing (..)
 
 updateFromFrontend : SessionId -> ClientId -> ToBackend -> Model -> ( Model, Cmd BackendMsg )
 updateFromFrontend sessionId clientId msg model =
@@ -55,5 +70,17 @@ updateFromFrontend sessionId clientId msg model =
 
 
 """
-                        ]
-        ]
+
+
+under1 =
+    """case msg of
+         CounterIncremented ->
+            let
+                newCounter =
+                    model.counter + 1
+            in
+            ( { model | counter = newCounter }, broadcast (CounterNewValue newCounter clientId) )"""
+
+
+
+-- TEST 2
