599255dbb2aa9a34b45ffa5c234b84a21346619d
James Carlson <jxxcarlson@gmail.com>
2024-06-03 03:53:42 +0200

    Oops: forgot to commit code

---
+++ tests/Install/FunctionBodyTest.elm
@@ -0,0 +1,61 @@
+module Install.FunctionBodyTest exposing (..)
+
+import Install.ClauseInCase
+import Install.FunctionBody
+import Run
+import Test exposing (Test, describe)
+
+
+all : Test
+all =
+    describe "Install.ClauseInCase"
+        [ Run.testFix test1
+        ]
+
+
+
+-- TEST 1
+
+
+test1 =
+    { description = "Test 1, replace function body of of Frontend.view"
+    , src = src1
+    , rule = rule1
+    , under = under1
+    , fixed = fixed1
+    , message = "Replace function \"view\""
+    }
+
+
+rule1 =
+    Install.FunctionBody.init
+        "Frontend"
+        "view"
+        """view model =
+   Html.text "This is a test\""""
+        |> Install.FunctionBody.makeRule
+
+
+src1 =
+    """module Frontend exposing(..)
+
+view model =
+   Html.div [] [ Html.text "Hello, World!" ]"""
+
+
+under1b =
+    """module Frontend exposing(..)
+
+"""
+
+
+under1 =
+    """view model =
+   Html.div [] [ Html.text "Hello, World!" ]"""
+
+
+fixed1 =
+    """module Frontend exposing(..)
+
+view model =
+   Html.text "This is a test\""""
