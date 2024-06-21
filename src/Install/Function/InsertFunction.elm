4db3c801d8e82ef75e5151873b28ac00199629d2
Mateus <mateusfpleite@gmail.com>
2024-06-21 09:20:55 -0300

    Install.Function: split insert and replace functionalities

---
+++ src/Install/Function/InsertFunction.elm
@@ -0,0 +1,159 @@
+module Install.Function.InsertFunction exposing (makeRule, init, Config, CustomError, withInsertAfter)
+
+{-| Add a function definition in a given module if it is not present.
+
+    -- code for ReviewConfig.elm:
+    rule =
+        Install.Function.InsertFunction.init
+            "Frontend"
+            "view"
+            """view model =
+
+Html.text "This is a test""""
+|> Install.Function.InsertFunction.makeRule
+
+Running this rule will insert the function `view` in the module `Frontend` with the provided implementation.
+
+The form of the rule is the same for nested modules:
+
+    rule =
+        Install.Function.InsertFunction.init
+            "Foo.Bar"
+            "earnInterest"
+            "hoho model = { model | interest = 1.03 * model.interest }"
+            |> Install.Function.InsertFunction.makeRule
+
+@docs makeRule, init, Config, CustomError, withInsertAfter
+
+-}
+
+import Elm.Syntax.Declaration exposing (Declaration(..))
+import Elm.Syntax.Expression exposing (Expression)
+import Elm.Syntax.ModuleName exposing (ModuleName)
+import Elm.Syntax.Node as Node exposing (Node)
+import Elm.Syntax.Range as Range exposing (Range)
+import Install.Infer as Infer
+import Install.Library
+import Install.Normalize as Normalize
+import Review.Fix as Fix
+import Review.ModuleNameLookupTable exposing (ModuleNameLookupTable)
+import Review.Rule as Rule exposing (Error, Rule)
+
+
+{-| Configuration for makeRule: add a function in a specified module if it does not already exist.
+-}
+type Config
+    = Config
+        { moduleName : String
+        , functionName : String
+        , functionImplementation : String
+        , theFunctionNodeExpression : Maybe (Node Expression)
+        , customErrorMessage : CustomError
+        , insertAt : InsertAt
+        }
+
+
+{-| Custom error message to be displayed when running `elm-review --fix` or `elm-review --fix-all`
+-}
+type CustomError
+    = CustomError { message : String, details : List String }
+
+
+type InsertAt
+    = After String
+    | AtEnd
+
+
+{-| Add the function after a specified declaration. Just give the name of a function, type, type alias or port and the function will be added after that declaration. Only work if the function is being added and not replaced.
+-}
+withInsertAfter : String -> Config -> Config
+withInsertAfter previousDeclaration (Config config) =
+    Config { config | insertAt = After previousDeclaration }
+
+
+{-| Initialize the configuration for the rule.
+-}
+init : String -> String -> String -> Config
+init moduleNaeme functionName functionImplementation =
+    Config
+        { moduleName = moduleNaeme
+        , functionName = functionName
+        , functionImplementation = functionImplementation
+        , theFunctionNodeExpression = Install.Library.maybeNodeExpressionFromString { moduleName = String.split "." moduleNaeme } functionImplementation
+        , customErrorMessage = CustomError { message = "Add function \"" ++ functionName ++ "\".", details = [ "" ] }
+        , insertAt = AtEnd
+        }
+
+
+{-| Create a rule that adds a function in a given module if it is not present.
+-}
+makeRule : Config -> Rule
+makeRule config =
+    let
+        visitor : Node Declaration -> Context -> ( List (Error {}), Context )
+        visitor declaration context =
+            declarationVisitor context config declaration
+    in
+    Rule.newModuleRuleSchemaUsingContextCreator "Install.Function.InsertFunction" initialContext
+        |> Rule.withDeclarationEnterVisitor visitor
+        |> Rule.withFinalModuleEvaluation (finalEvaluation config)
+        |> Rule.providesFixesForModuleRule
+        |> Rule.fromModuleRuleSchema
+
+
+type alias Context =
+    { moduleName : ModuleName
+    , lookupTable : ModuleNameLookupTable
+    , lastDeclarationRange : Range
+    }
+
+
+initialContext : Rule.ContextCreator () Context
+initialContext =
+    Rule.initContextCreator
+        (\lookupTable moduleName () -> { lookupTable = lookupTable, moduleName = moduleName, lastDeclarationRange = Range.empty })
+        |> Rule.withModuleNameLookupTable
+        |> Rule.withModuleName
+
+
+declarationVisitor : Context -> Config -> Node Declaration -> ( List (Rule.Error {}), Context )
+declarationVisitor context (Config config) declaration =
+    let
+        contextWithLastDeclarationRange =
+            case config.insertAt of
+                After previousDeclaration ->
+                    if Install.Library.getDeclarationName declaration == previousDeclaration then
+                        { context | lastDeclarationRange = Node.range declaration }
+
+                    else
+                        context
+
+                AtEnd ->
+                    { context | lastDeclarationRange = Node.range declaration }
+    in
+    ( [], contextWithLastDeclarationRange )
+
+
+finalEvaluation : Config -> Context -> List (Rule.Error {})
+finalEvaluation (Config config) context =
+    if Install.Library.isInCorrectModule config.moduleName context then
+        addFunction { range = context.lastDeclarationRange, functionName = config.functionName, functionImplementation = config.functionImplementation }
+
+    else
+        []
+
+
+type alias FixConfig =
+    { range : Range
+    , functionName : String
+    , functionImplementation : String
+    }
+
+
+addFunction : FixConfig -> List (Error {})
+addFunction fixConfig =
+    [ Rule.errorWithFix
+        { message = "Add function \"" ++ fixConfig.functionName ++ "\"", details = [ "" ] }
+        fixConfig.range
+        [ Fix.insertAt { row = fixConfig.range.end.row + 1, column = 0 } fixConfig.functionImplementation ]
+    ]
--- src/Install/Function/ReplaceFunction.elm
+++ src/Install/Function/ReplaceFunction.elm
@@ -1,28 +1,29 @@
-module Install.Function exposing (makeRule, init, Config, CustomError, withInsertAfter)
+module Install.Function.ReplaceFunction exposing (makeRule, init, Config, CustomError)
 
-{-| Replace a function in a given module with a new implementation or
-add that function definition if it is not present in the module.
+{-| Replace a function in a given module with a new implementation.
 
     -- code for ReviewConfig.elm:
     rule =
-        Install.Function.init
+        Install.Function.ReplaceFunction.init
             "Frontend"
             "view"
             """view model =
-       Html.text "This is a test\""""
-            |> Install.Function.makeRule
 
-Running this rule will insert or replace the function `view` in the module `Frontend` with the new implementation.
+Html.text "This is a test""""
+|> Install.Function.ReplaceFunction.makeRule
+
+Running this rule will replace the function `view` in the module `Frontend` with the provided implementation.
+
 The form of the rule is the same for nested modules:
 
     rule =
-        Install.Function.init
+        Install.Function.ReplaceFunction.init
             "Foo.Bar"
             "earnInterest"
             "hoho model = { model | interest = 1.03 * model.interest }"
-            |> Install.Function.makeRule
+            |> Install.Function.ReplaceFunction.makeRule
 
-@docs makeRule, init, Config, CustomError, withInsertAfter
+@docs makeRule, init, Config, CustomError
 
 -}
 
@@ -39,7 +40,7 @@ import Review.ModuleNameLookupTable exposing (ModuleNameLookupTable)
 import Review.Rule as Rule exposing (Error, Rule)
 
 
-{-| Configuration for makeRule: add (or replace if function already exists) a function in a specified module.
+{-| Configuration for makeRule: replace a function in a specified module with a new implementation.
 -}
 type Config
     = Config
@@ -48,7 +49,6 @@ type Config
         , functionImplementation : String
         , theFunctionNodeExpression : Maybe (Node Expression)
         , customErrorMessage : CustomError
-        , insertAt : InsertAt
         }
 
 
@@ -56,18 +56,6 @@ type Config
 -}
 type CustomError
     = CustomError { message : String, details : List String }
-
-
-type InsertAt
-    = After String
-    | AtEnd
-
-
-{-| Add the function after a specified declaration. Just give the name of a function, type, type alias or port and the function will be added after that declaration. Only work if the function is being added and not replaced.
--}
-withInsertAfter : String -> Config -> Config
-withInsertAfter previousDeclaration (Config config) =
-    Config { config | insertAt = After previousDeclaration }
 
 
 {-| Initialize the configuration for the rule.
@@ -80,12 +68,10 @@ init moduleNaeme functionName functionImplementation =
         , functionImplementation = functionImplementation
         , theFunctionNodeExpression = Install.Library.maybeNodeExpressionFromString { moduleName = String.split "." moduleNaeme } functionImplementation
         , customErrorMessage = CustomError { message = "Replace function \"" ++ functionName ++ "\" with new code.", details = [ "" ] }
-        , insertAt = AtEnd
         }
 
 
-{-| Create a rule that replaces a function in a given module with a new implementation or
-creates it if it is not present.
+{-| Create a rule that replaces a function in a given module with a new implementation.
 -}
 makeRule : Config -> Rule
 makeRule config =
@@ -94,9 +80,8 @@ makeRule config =
         visitor declaration context =
             declarationVisitor context config declaration
     in
-    Rule.newModuleRuleSchemaUsingContextCreator "Install.Function" initialContext
+    Rule.newModuleRuleSchemaUsingContextCreator "Install.Function.ReplaceFunction" initialContext
         |> Rule.withDeclarationEnterVisitor visitor
-        |> Rule.withFinalModuleEvaluation (finalEvaluation config)
         |> Rule.providesFixesForModuleRule
         |> Rule.fromModuleRuleSchema
 
@@ -104,34 +89,19 @@ makeRule config =
 type alias Context =
     { moduleName : ModuleName
     , lookupTable : ModuleNameLookupTable
-    , lastDeclarationRange : Range
-    , appliedFix : Bool
     }
 
 
 initialContext : Rule.ContextCreator () Context
 initialContext =
     Rule.initContextCreator
-        (\lookupTable moduleName () -> { lookupTable = lookupTable, moduleName = moduleName, lastDeclarationRange = Range.empty, appliedFix = False })
+        (\lookupTable moduleName () -> { lookupTable = lookupTable, moduleName = moduleName })
         |> Rule.withModuleNameLookupTable
         |> Rule.withModuleName
 
 
 declarationVisitor : Context -> Config -> Node Declaration -> ( List (Rule.Error {}), Context )
 declarationVisitor context (Config config) declaration =
-    let
-        contextWithLastDeclarationRange =
-            case config.insertAt of
-                After previousDeclaration ->
-                    if getDeclarationName declaration == previousDeclaration then
-                        { context | lastDeclarationRange = Node.range declaration }
-
-                    else
-                        context
-
-                AtEnd ->
-                    { context | lastDeclarationRange = Node.range declaration }
-    in
     case Node.value declaration of
         FunctionDeclaration function ->
             let
@@ -145,45 +115,27 @@ declarationVisitor context (Config config) declaration =
                 resources =
                     { lookupTable = context.lookupTable, inferredConstants = ( Infer.empty, [] ) }
 
-                -- isNotImplemented returns True if the values of the current function expression and the replacement expression are different
+                isInCorrectFunction =
+                    isInCorrectModule && name == config.functionName
+
                 isNotImplemented : Function -> { a | functionImplementation : String } -> Bool
                 isNotImplemented f confg =
-                    Maybe.map2 (Normalize.compare resources)
-                        (f.declaration |> Node.value |> .expression |> Just)
-                        (Install.Library.getExpressionFromString context confg.functionImplementation)
-                        == Just Normalize.ConfirmedEquality
-                        |> not
-
-                isImplemented =
-                    not (isNotImplemented function config)
+                    isInCorrectFunction
+                        && (Maybe.map2 (Normalize.compare resources)
+                                (f.declaration |> Node.value |> .expression |> Just)
+                                (Install.Library.getExpressionFromString context confg.functionImplementation)
+                                == Just Normalize.ConfirmedEquality
+                                |> not
+                           )
             in
-            if isImplemented then
-                ( [], { contextWithLastDeclarationRange | appliedFix = True } )
-
-            else if name == config.functionName && isInCorrectModule && isNotImplemented function config then
+            if isNotImplemented function config then
                 replaceFunction { range = Node.range declaration, functionName = config.functionName, functionImplementation = config.functionImplementation } context
 
             else
-                ( [], contextWithLastDeclarationRange )
+                ( [], context )
 
         _ ->
-            ( [], contextWithLastDeclarationRange )
-
-
-type alias FixConfig =
-    { range : Range
-    , functionName : String
-    , functionImplementation : String
-    }
-
-
-finalEvaluation : Config -> Context -> List (Rule.Error {})
-finalEvaluation (Config config) context =
-    if not context.appliedFix && Install.Library.isInCorrectModule config.moduleName context then
-        addFunction { range = context.lastDeclarationRange, functionName = config.functionName, functionImplementation = config.functionImplementation }
-
-    else
-        []
+            ( [], context )
 
 
 replaceFunction : FixConfig -> Context -> ( List (Error {}), Context )
@@ -193,17 +145,8 @@ replaceFunction fixConfig context =
             fixConfig.range
             [ Fix.replaceRangeBy fixConfig.range fixConfig.functionImplementation ]
       ]
-    , { context | appliedFix = True }
+    , context
     )
-
-
-addFunction : FixConfig -> List (Error {})
-addFunction fixConfig =
-    [ Rule.errorWithFix
-        { message = "Add function \"" ++ fixConfig.functionName ++ "\"", details = [ "" ] }
-        fixConfig.range
-        [ Fix.insertAt { row = fixConfig.range.end.row + 1, column = 0 } fixConfig.functionImplementation ]
-    ]
 
 
 getDeclarationName : Node Declaration -> String
@@ -227,3 +170,10 @@ getDeclarationName declaration =
 
         _ ->
             ""
+
+
+type alias FixConfig =
+    { range : Range
+    , functionName : String
+    , functionImplementation : String
+    }
