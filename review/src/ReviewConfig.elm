cf319abee5e95343a54a75a93f4e635971399caa
Mateus <mateusfpleite@gmail.com>
2024-07-18 22:42:38 -0300

    add new rules to review

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -1,6 +1,10 @@
 module ReviewConfig exposing (config)
 
 import NoDebug.Log
+import Docs.NoMissing exposing (exposedModules, onlyExposed)
+import Docs.ReviewAtDocs
+import Docs.ReviewLinksAndSections
+import Docs.UpToDateReadmeLinks
 import NoDebug.TodoOrToString
 import NoUnused.Dependencies
 import NoUnused.Variables
@@ -9,8 +13,11 @@ import Review.Rule exposing (Rule)
 
 config : List Rule
 config =
-    [ NoUnused.Dependencies.rule
-    , NoUnused.Variables.rule
-
-    --, SetupProgramTest.rule
+    [ NoUnused.Variables.rule
+    , Docs.NoMissing.rule
+        { document = onlyExposed
+        , from = exposedModules
+        }
+    , Docs.ReviewLinksAndSections.rule
+    , Docs.ReviewAtDocs.rule
     ]
